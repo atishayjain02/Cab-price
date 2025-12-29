ffd23186174f9bb1e2d3cc6b428b8f69dc2ca9d9
Twiisttt <rai.tejasvi0@gmail.com>
2025-12-30 00:10:42 +0530

    Day 2: Add /compare POST API to receive JSON input

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask,jsonify
+print("APP.PY LOADED")
+from flask import Flask,jsonify,request
 
 app = Flask(__name__)
 
@@ -9,6 +10,15 @@ def home():
 @app.route("/status")
 def status():
     return jsonify({"status":"ok"})
+
+@app.route("/compare",methods=["POST"])
+def compare():
+     print("COMPARE ROUTE HIT")
+     data = request.json
+     return jsonify({
+        "message":"data received successfully",
+        "data":data
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
